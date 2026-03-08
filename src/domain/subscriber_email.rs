51fcc9fa8f1c07b3b2226603e39aa67a7bf10b9c
valkyraycho <razor0424@gmail.com>
2026-03-08 12:33:20 +0800

    feat: add subscriber email validation

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,51 @@
+use validator::ValidateEmail;
+
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        match s.validate_email() {
+            true => Ok(Self(s)),
+            false => Err(format!("{} is not a valid subscriber email.", s)),
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claim::{assert_err, assert_ok};
+    use fake::{Fake, faker::internet::en::SafeEmail};
+
+    use super::SubscriberEmail;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursulaexample.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@domain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email: String = SafeEmail().fake();
+        assert_ok!(SubscriberEmail::parse(email));
+    }
+}
